a18540d2400332de5a5f84549b89069f3a77ce1e
Randy Klose <randyklose@gmail.com>
2017-01-14 19:25:26 +0000

    Real data workshop completed

--- answers.js
+++ answers.js
@@ -1502,7 +1502,7 @@ function isItGoingToRain() {
            acc = true;
        }
         return acc;
-    }, "");
+    }, false);
 }
 
 console.log(isItGoingToRain());
@@ -1519,11 +1519,9 @@ function isItGoingToRainEigth() {
        if(currentHour.icon === "rain") {
            acc = true;
        } 
-       else {
-           acc= false;
-       }
+      
           return acc;
-    }, "");
+    }, false);
 }
 
 console.log(isItGoingToRainEigth());
@@ -1535,7 +1533,13 @@ elements as the original array.
 
 Example output: `[72.9, 70.5, ...]`*/
 
+function onlyTemperatures() {
+    return dataset.hourly.data.map(function(object){
+        return object.temperature;
+    });
+}
 
+console.log(onlyTemperatures());
 
 /*## When is it going to rain?
 Using a chain of two array methods, start with the hourly data and return an array of 
@@ -1544,16 +1548,54 @@ Using a chain of two array methods, start with the hourly data and return an arr
 1. Eliminate any item where it is not raining.
 2. Extract the time from the remaining items and create a `Date` object*/
 
+function whenRain() {
+    return dataset.hourly.data.filter(function(object){
+      if  (object.icon ==="rain") {
+          return true;
+      }
+      else {
+          return false;
+      }
+    }).map(function(object) {
+        return new Date(object.time * 1000);
+    })
+}
+
+console.log(whenRain());
+
 /*## Is it going to be sunny all week?
 Using the appropriate array method, start with the **daily** data and
 figure out whether it will be sunny **every day** of the week.
 Use the `summary` and check if it contains the word `"sun"` using your preferred method.*/
+
+function sunnyEveryDay() {
+    
+    return dataset.daily.data.every(function(object) {
+        if(object.summary === "sun") {
+            return true;
+        }
+    });
+}
+
+console.log(sunnyEveryDay());
 
 /*## How hot will it be today?
 Using the appropriate array method, start with the hourly data and figure out
 what will be **the hottest** temperature today. 
 As a hint, notice that you will need to visit each item in turn,
 but also **keep track of a separate value**.*/
+
+function hottest() {
+    return dataset.hourly.data.reduce(function(acc, object) {
+        if( object.temperature > acc) {
+            acc = object.temperature;
+        }
+        return acc;
+    }, 0)
+}
+
+console.log(hottest());
+
 
 /*## Stats on this week's weather
 Using the appropriate array method, start with the **daily** data and
@@ -1569,3 +1611,306 @@ An example output could be:
 }
 ```
 */
+
+function dailyIcon() {
+    return dataset.daily.data.reduce(function(acc, object) {
+       
+        if(object.icon === "cloudy"){
+            acc["cloudy"] ++; 
+        }
+        else if(object.icon === "rain"){
+            acc["rain"] ++; 
+        }
+        else if(object.icon === "partly-cloudy-night"){
+            acc["partly-cloudy-night"] ++; 
+        }
+        else if(object.icon === "cloudy"){
+            acc["sunny"] ++; 
+        }
+        else if(object.icon === "partly-cloudy-day"){
+            acc["partly-cloudy-day"] ++; 
+        }
+        return acc;
+    }, {"rain":0, "cloudy":0, "partly-cloudy-night":0, "sunny":0, "partly-cloudy-day":0});
+}
+
+console.log(dailyIcon());
+
+/*## A long happy chain
+
+Here is the data for this exercise:
+
+```json*/
+var data = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.biz",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Gwenborough",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+  {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "address": {
+      "street": "Douglas Extension",
+      "suite": "Suite 847",
+      "city": "McKenziehaven",
+      "zipcode": "59590-4157",
+      "geo": {
+        "lat": "-68.6102",
+        "lng": "-47.0653"
+      }
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+      "name": "Romaguera-Jacobson",
+      "catchPhrase": "Face to face bifurcated interface",
+      "bs": "e-enable strategic applications"
+    }
+  },
+  {
+    "id": 4,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "email": "Julianne.OConner@kory.org",
+    "address": {
+      "street": "Hoeger Mall",
+      "suite": "Apt. 692",
+      "city": "South Elvis",
+      "zipcode": "53919-4257",
+      "geo": {
+        "lat": "29.4572",
+        "lng": "-164.2990"
+      }
+    },
+    "phone": "493-170-9623 x156",
+    "website": "kale.biz",
+    "company": {
+      "name": "KittyTown",
+      "catchPhrase": "Multi-tiered zero tolerance productivity",
+      "bs": "transition cutting-edge web services"
+    }
+  },
+  {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "email": "Lucio_Hettinger@annie.biz",
+    "address": {
+      "street": "Skiles Walks",
+      "suite": "Suite 351",
+      "city": "KittyTown",
+      "zipcode": "33263",
+      "geo": {
+        "lat": "-31.8129",
+        "lng": "62.5342"
+      }
+    },
+    "phone": "(254)954-1289",
+    "website": "demarco.info",
+    "company": {
+      "name": "Keebler LLC",
+      "catchPhrase": "User-centric fault-tolerant solution",
+      "bs": "revolutionize end-to-end systems"
+    }
+  },
+  {
+    "id": 6,
+    "name": "Mrs. Dennis Schulist",
+    "username": "Leopoldo_Corkery",
+    "email": "Karley_Dach@jasper.info",
+    "address": {
+      "street": "Norberto Crossing",
+      "suite": "Apt. 950",
+      "city": "South Christy",
+      "zipcode": "23505-1337",
+      "geo": {
+        "lat": "-71.4197",
+        "lng": "71.7478"
+      }
+    },
+    "phone": "1-477-935-8478 x6430",
+    "website": "ola.org",
+    "company": {
+      "name": "Considine-Lockman",
+      "catchPhrase": "Synchronised bottom-line interface",
+      "bs": "e-enable innovative applications"
+    }
+  },
+  {
+    "id": 7,
+    "name": "Kurtis Weissnat",
+    "username": "Elwyn.Skiles",
+    "email": "Telly.Hoeger@billy.biz",
+    "address": {
+      "street": "Rex Trail",
+      "suite": "Suite 280",
+      "city": "Howemouth",
+      "zipcode": "58804-1099",
+      "geo": {
+        "lat": "24.8918",
+        "lng": "21.8984"
+      }
+    },
+    "phone": "210.067.6132",
+    "website": "elvis.io",
+    "company": {
+      "name": "Johns Group",
+      "catchPhrase": "Configurable multimedia task-force",
+      "bs": "generate enterprise e-tailers"
+    }
+  },
+  {
+    "id": 8,
+    "name": "Nicholas Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "email": "Sherwood@rosamond.biz",
+    "address": {
+      "street": "Ellsworth Summit",
+      "suite": "Suite 729",
+      "city": "MeowTown",
+      "zipcode": "45169",
+      "geo": {
+        "lat": "-14.3990",
+        "lng": "-120.7677"
+      }
+    },
+    "phone": "586.493.6943 x140",
+    "website": "jacynthe.com",
+    "company": {
+      "name": "Abernathy Group",
+      "catchPhrase": "Implemented secondary concept",
+      "bs": "e-enable extensible e-tailers"
+    }
+  },
+  {
+    "id": 9,
+    "name": "Glenna Reichert",
+    "username": "Delphine",
+    "email": "Chaim_McDermott@dana.io",
+    "address": {
+      "street": "Dayna Park",
+      "suite": "Suite 449",
+      "city": "Bartholomebury",
+      "zipcode": "76495-3109",
+      "geo": {
+        "lat": "24.6463",
+        "lng": "-168.8889"
+      }
+    },
+    "phone": "(775)976-6794 x41206",
+    "website": "conrad.com",
+    "company": {
+      "name": "Yost and Sons",
+      "catchPhrase": "Switchable contextually-based project",
+      "bs": "aggregate real-time technologies"
+    }
+  },
+  {
+    "id": 10,
+    "name": "Clementina DuBuque",
+    "username": "Moriah.Stanton",
+    "email": "Rey.Padberg@karina.biz",
+    "address": {
+      "street": "Kattie Turnpike",
+      "suite": "Suite 198",
+      "city": "Lebsackbury",
+      "zipcode": "31428-2261",
+      "geo": {
+        "lat": "-38.2386",
+        "lng": "57.2232"
+      }
+    },
+    "phone": "024-648-3804",
+    "website": "ambrose.net",
+    "company": {
+      "name": "Hoeger LLC",
+      "catchPhrase": "Centralized empowering task-force",
+      "bs": "target end-to-end models"
+    }
+  }
+]
+
+
+/*Using a chain of **four** array methods, do the following:
+
+1. Find out all the people who have .biz emails
+2. Extract the city where they live
+3. Keep only the unique cities in the list (hint: this is NOT `filter`)
+4. Print the remaining data on the screen
+
+As an additional hint, the first method is `filter` and the last one is `forEach`. Up to you to figure out the two middle ones.
+
+Here is how your code will look. Fill in the blanks...
+
+```javascript
+data*/
+function fourMethods() {
+    return data.filter(function(object) {
+        if (object.email.slice(-4) === ".biz") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    })
+    .map(function(object1) {
+        return object1.address.city;
+    })
+    .reduce(function(acc, city) {
+        console.log(acc);
+        if(acc.indexOf(city) === -1) {
+            acc.push(city)
+        }
+        return acc;
+    }, [])
+    .forEach(function(object3) {
+        console.log(object3);
+    
+    });
+}
+
+console.log(fourMethods());
